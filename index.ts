1e24a33699e8adc1f1ff44ba8f0e216c04d51a76
Sumeet Agarwal <sumeet.a@gmail.com>
2024-09-09 17:35:58 -0700

    wip

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ function code(code: string, filename: string) {
         filename,
         code,
         ts.ScriptTarget.ESNext,
-        true,
+        true
     );
     codes.push(sourceFile);
     return sourceFile;
@@ -22,7 +22,7 @@ x.
 
 let y = x();
 `,
-    "main.js",
+    "main.js"
 );
 
 // stdlib
@@ -34,26 +34,26 @@ declare var console: {
   // Add other console methods as needed
 };
   `,
-    "console.d.ts",
+    "console.d.ts"
 );
 
 code(
     fs.readFileSync(require.resolve("typescript/lib/lib.es5.d.ts"), "utf-8"),
-    "lib.es5.d.ts",
+    "lib.es5.d.ts"
 );
 code(
     fs.readFileSync(
         require.resolve("typescript/lib/lib.es2015.core.d.ts"),
-        "utf-8",
+        "utf-8"
     ),
-    "lib.es2015.core.d.ts",
+    "lib.es2015.core.d.ts"
 );
 code(
     fs.readFileSync(
         require.resolve("typescript/lib/lib.es2019.array.d.ts"),
-        "utf-8",
+        "utf-8"
     ),
-    "lib.es2019.array.d.ts",
+    "lib.es2019.array.d.ts"
 );
 
 // sup lib .d.ts (will be pulled in from the MAP service)
@@ -123,7 +123,7 @@ const languageService = ts.createLanguageService(
         readFile: () => undefined,
         fileExists: (fileName) => codes.some((c) => c.fileName === fileName),
     },
-    ts.createDocumentRegistry(),
+    ts.createDocumentRegistry()
 );
 
 // Find the position of "x."
@@ -133,86 +133,129 @@ const position = main.getText().lastIndexOf("x.") + 2;
 const completions = languageService.getCompletionsAtPosition(
     "main.js",
     position,
-    {includeSymbol: true},
+    { includeSymbol: true }
 );
+
+function resolveType(
+    typeChecker: ts.TypeChecker,
+    type: ts.Type,
+    contextType: ts.Type
+): ts.Type {
+    if (type.isTypeParameter()) {
+        const typeParameter = type as ts.TypeParameter;
+        const typeReference = contextType as ts.TypeReference;
+
+        if (typeReference.target && typeReference.typeArguments) {
+            const typeParamIndex =
+                typeReference.target.typeParameters?.findIndex(
+                    (param) => param.symbol.name === typeParameter.symbol.name
+                );
+
+            if (typeParamIndex !== undefined && typeParamIndex >= 0) {
+                return typeReference.typeArguments[typeParamIndex] || type;
+            }
+        }
+    }
+
+    if (type.flags & ts.TypeFlags.ThisType) {
+        return contextType;
+    }
+
+    return type;
+}
 
 if (completions && completions.entries.length > 0) {
     const typeChecker = program.getTypeChecker();
-
-    // completions.entries.forEach((entry) => {
-    //     if (
-    //         entry.kind === ts.ScriptElementKind.functionElement ||
-    //         entry.kind === ts.ScriptElementKind.memberFunctionElement
-    //     ) {
-    //     }
-    // });
 
     for (const completion of completions.entries) {
         console.log(`Completion: ${completion.name}`);
         const symbol = completion.symbol;
         if (symbol && symbol.valueDeclaration) {
-            const symbolType = typeChecker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration);
-            const typeString = typeChecker.typeToString(symbolType, undefined, ts.TypeFormatFlags.OmitThisParameter);
-            console.log(`Type: ${typeString}`);
+            const symbolType = typeChecker.getTypeOfSymbolAtLocation(
+                symbol,
+                symbol.valueDeclaration
+            );
+
+            const contextType =
+                typeChecker.getContextualType(symbol.valueDeclaration.parent) ||
+                symbolType;
+
+            let result = {
+                type: "attr",
+                returns: "",
+                params: [],
+            };
+
+            const signatures = typeChecker.getSignaturesOfType(
+                symbolType,
+                ts.SignatureKind.Call
+            );
+            if (signatures.length > 0) {
+                result.type = "function";
+
+                const signature = signatures[0];
+                const returnType =
+                    typeChecker.getReturnTypeOfSignature(signature);
+
+                const resolvedReturnType = resolveType(
+                    typeChecker,
+                    returnType,
+                    contextType
+                );
+
+                result.returns = typeChecker.typeToString(resolvedReturnType);
+
+                result.params = signature.parameters.map((param) => {
+                    const paramType = typeChecker.getTypeOfSymbolAtLocation(
+                        param,
+                        param.valueDeclaration || param.declarations[0]
+                    );
+                    const resolvedParamType = resolveType(
+                        typeChecker,
+                        paramType,
+                        contextType
+                    );
+
+                    return {
+                        name: param.getName(),
+                        type: typeChecker.typeToString(resolvedParamType),
+                    };
+                });
+            }
+
+            console.log(result);
         }
     }
 } else {
     console.log("No completions found.");
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function x() {
-            // const details = languageService.getCompletionEntryDetails(
-            //     "main.js",
-            //     position,
-            //     entry.name,
-            //     undefined,
-            //     undefined,
-            //     undefined,
-            //     undefined,
-            // );
-
-            // if (details) {
-            //     console.log(`Function: ${entry.name}`);
-            //     const sourceFile = program.getSourceFile("main.js");
-
-            //     if (details.documentation) {
-            //         console.log(
-            //             `Documentation: ${ts.displayPartsToString(details.documentation)}`,
-            //         );
-            //     } else {
-            //         console.log("No documentation found.");
-            //     }
-
-            //     if (details.tags) {
-            //         for (const tag of details.tags) {
-            //             console.log(`Tag`, tag);
-            //         }
-            //     } else {
-            //         console.log("No tags found.");
-            //     }
-            // }
+    // const details = languageService.getCompletionEntryDetails(
+    //     "main.js",
+    //     position,
+    //     entry.name,
+    //     undefined,
+    //     undefined,
+    //     undefined,
+    //     undefined,
+    // );
+    // if (details) {
+    //     console.log(`Function: ${entry.name}`);
+    //     const sourceFile = program.getSourceFile("main.js");
+    //     if (details.documentation) {
+    //         console.log(
+    //             `Documentation: ${ts.displayPartsToString(details.documentation)}`,
+    //         );
+    //     } else {
+    //         console.log("No documentation found.");
+    //     }
+    //     if (details.tags) {
+    //         for (const tag of details.tags) {
+    //             console.log(`Tag`, tag);
+    //         }
+    //     } else {
+    //         console.log("No tags found.");
+    //     }
+    // }
 }
